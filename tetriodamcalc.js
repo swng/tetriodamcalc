3611d35f6726592e3c09913f8cd304afada3dc47
swng <swng314@gmail.com>
2021-05-24 13:45:00 -0500

    PCs are distinguished as separate damage

--- tetriodamcalc.js
+++ tetriodamcalc.js
@@ -3,71 +3,70 @@ var num_attacks = 9;
 renderFields();
 
 function addField() {
-    num_attacks++;
-    renderFields();
+	num_attacks++;
+	renderFields();
 }
 function removeField() {
-    num_attacks--;
-    if (num_attacks < 1) num_attacks = 1;
-    renderFields();
+	num_attacks--;
+	if (num_attacks < 1) num_attacks = 1;
+	renderFields();
 }
 
 function renderFields() {
-    var container = document.getElementById("container");
-    while (num_rendered > num_attacks) {
-        for (i = 0; i < 7; i++)  container.removeChild(container.lastChild);
-        num_rendered--;
-    }
-    for (i = num_rendered; i < num_attacks; i++) {
-        var attackType = document.createElement("select");
-        attackType.id = "attack type " + i;
-        var attackTypeLabel = document.createElement("label");
-        attackTypeLabel.htmlFor = attackType.id;
-        attackTypeLabel.innerText = "Attack type: ";
-        var PC = document.createElement("input");
-        PC.id = "PC " + i;
-        PC.type = "checkbox";
-        var PCLabel = document.createElement("label");
-        PCLabel.htmlFor = PC.id;
-        PCLabel.innerText = " PC? ";
+	var container = document.getElementById('container');
+	while (num_rendered > num_attacks) {
+		for (i = 0; i < 7; i++) container.removeChild(container.lastChild);
+		num_rendered--;
+	}
+	for (i = num_rendered; i < num_attacks; i++) {
+		var attackType = document.createElement('select');
+		attackType.id = 'attack type ' + i;
+		var attackTypeLabel = document.createElement('label');
+		attackTypeLabel.htmlFor = attackType.id;
+		attackTypeLabel.innerText = 'Attack type: ';
+		var PC = document.createElement('input');
+		PC.id = 'PC ' + i;
+		PC.type = 'checkbox';
+		var PCLabel = document.createElement('label');
+		PCLabel.htmlFor = PC.id;
+		PCLabel.innerText = ' PC? ';
 
-        attackType.append(new Option("None", -1));
-        attackType.append(new Option("Single", 0));
-        attackType.append(new Option("Double", 1));
-        attackType.append(new Option("Triple", 2));
-        attackType.append(new Option("Quad", 3));
-        attackType.append(new Option("T-Spin Mini Single", 4));
-        attackType.append(new Option("T-Spin Single", 5));
-        attackType.append(new Option("T-Spin Mini Double", 6));
-        attackType.append(new Option("T-Spin Double", 7));
-        attackType.append(new Option("T-Spin Triple", 8));
+		attackType.append(new Option('None', -1));
+		attackType.append(new Option('Single', 0));
+		attackType.append(new Option('Double', 1));
+		attackType.append(new Option('Triple', 2));
+		attackType.append(new Option('Quad', 3));
+		attackType.append(new Option('T-Spin Mini Single', 4));
+		attackType.append(new Option('T-Spin Single', 5));
+		attackType.append(new Option('T-Spin Mini Double', 6));
+		attackType.append(new Option('T-Spin Double', 7));
+		attackType.append(new Option('T-Spin Triple', 8));
 
-        var damage_0 = document.createElement("span");
-        damage_0.innerText = " Damage: ";
-        var damage_1 = document.createElement("span");
-        damage_1.id = "damage " + i;
-        damage_1.innerText = 0;
-            
-        container.appendChild(attackTypeLabel);
-        container.appendChild(attackType);
-        container.appendChild(PCLabel);
-        container.appendChild(PC);
-        container.appendChild(damage_0);
-        container.appendChild(damage_1);
+		var damage_0 = document.createElement('span');
+		damage_0.innerText = ' Damage: ';
+		var damage_1 = document.createElement('span');
+		damage_1.id = 'damage ' + i;
+		damage_1.innerText = 0;
 
+		container.appendChild(attackTypeLabel);
+		container.appendChild(attackType);
+		container.appendChild(PCLabel);
+		container.appendChild(PC);
+		container.appendChild(damage_0);
+		container.appendChild(damage_1);
 
-        container.appendChild(document.createElement("p"));
-        num_rendered++;
-            
-    }
+		container.appendChild(document.createElement('p'));
+		num_rendered++;
+	}
 }
 
 function showAttackTable() {
-    console.log(AttackTable);
-    alert("just use console to view or edit AttackTable\nbtw it doesn't set until clicking calculate\n " + JSON.stringify(AttackTable));
+	console.log(AttackTable);
+	alert(
+		"just use console to view or edit AttackTable\nbtw it doesn't set until clicking calculate\n" +
+			JSON.stringify(AttackTable)
+	);
 }
-
-
 
 function B2B_level(B2B) {
 	// source: osk: https://discord.com/channels/673303546107658242/674421736162197515/713419086486437960
@@ -89,7 +88,7 @@ function B2B_level(B2B) {
 var AttackTable1 = [
 	// straight taken from osk's table: https://cdn.discordapp.com/attachments/674421736162197515/716081165886423110/2020-05-30_02-07-18.png
 	[
-        [0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3], // Single // hits 4 at... 43
+		[0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3], // Single // hits 4 at... 43
 		[1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6], // Double. Increments every 4 combos.
 		[2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12], // Triple. Increments every 2 combos.
 		[4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24], // Quad. Increments every combo.
@@ -177,7 +176,10 @@ var AttackTable2 = [
 
     */
 	[
-        [0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4], // Single // hits 4 at... 43
+		[
+			0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+			3, 3, 3, 3, 3, 3, 3, 4,
+		], // Single // hits 4 at... 43
 		[1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6], // Double. Increments every 4 combos.
 		[2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 12], // Triple. Increments every 2 combos.
 		[4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24], // Quad. Increments every combo.
@@ -245,61 +247,60 @@ function attackTable(type, combo, B2B) {
 	if (type == -1) return 0;
 	if (type < 3) B2B = 0; // these two sanity checks shouldn't be necessary and are handled at the upper level but... redundancy?
 
-    let B2B_Level = B2B_level(B2B);
+	let B2B_Level = B2B_level(B2B);
 
-    if (B2B_Level < AttackTable1.length) {
-        if (combo < 21) return AttackTable[B2B_Level][type][combo]; // default behaviour: under 21c we can directly reference the table
-        
-        if (B2B_Level == 0 && (type == 0 || type == 4)) { // these two are nonlinear but it is known that it hits 4 damage at 43 combo
-            if (combo >= 43) return 4;
-            return AttackTable[B2B_Level][type][20];
-        }
+	if (B2B_Level < AttackTable1.length) {
+		if (combo < 21) return AttackTable[B2B_Level][type][combo]; // default behaviour: under 21c we can directly reference the table
 
-        LE = linear(AttackTable[B2B_Level][type]); // attempt to extrapolate linear pattern
-        if (LE.length == 3) {
-            return LE[0] * combo + Math.floor(combo / LE[1]) + LE[2];
-        }
+		if (B2B_Level == 0 && (type == 0 || type == 4)) {
+			// these two are nonlinear but it is known that it hits 4 damage at 43 combo
+			if (combo >= 43) return 4;
+			return AttackTable[B2B_Level][type][20];
+		}
 
-        return AttackTable[B2B_Level][type][20]; // default to final value in the table
-                                                 // or we can default to [B2B_0 + additive_b2b_level] math instead, would that be better??
+		LE = linear(AttackTable[B2B_Level][type]); // attempt to extrapolate linear pattern
+		if (LE.length == 3) {
+			return LE[0] * combo + Math.floor(combo / LE[1]) + LE[2];
+		}
 
-    }
+		return AttackTable[B2B_Level][type][20]; // default to final value in the table
+		// or we can default to [B2B_0 + additive_b2b_level] math instead, would that be better??
+	}
 
 	return attackTable(type, combo, 0) + B2B_Level; // past the mapped table we fall back on the math of additive B2B level
 }
 
-
 function verify(list, a, b, c) {
-    for (i = 1; i < list.length; i++) {
-        if (a*i + Math.floor(i/b) + c != list[i]) return false;
-    }
-    return true;
+	for (i = 1; i < list.length; i++) {
+		if (a * i + Math.floor(i / b) + c != list[i]) return false;
+	}
+	return true;
 }
 let memoized_linears = {};
 function linear(list) {
-    if (list in memoized_linears) { // caching results
-        return memoized_linears[list];
-    }
-    for (a = 0; a < 200; a++) {
-        for (b = -20; b < 20; b++) {
-            for (c = 0; c < 200; c++) {
-                if (verify(list, a, b, c)) {
-                    memoized_linears[list] = [a, b, c];
-                    return [a,b,c];
-                }
-            }
-        }
-    }
-    memoized_linears[list] = [];
-    return [];
+	if (list in memoized_linears) {
+		// caching results
+		return memoized_linears[list];
+	}
+	for (a = 0; a < 200; a++) {
+		for (b = -20; b < 20; b++) {
+			for (c = 0; c < 200; c++) {
+				if (verify(list, a, b, c)) {
+					memoized_linears[list] = [a, b, c];
+					return [a, b, c];
+				}
+			}
+		}
+	}
+	memoized_linears[list] = [];
+	return [];
 }
 
 function calculate() {
-    table_used = document.getElementById('attack table').value;
-    if (table_used == 1) AttackTable = AttackTable1;
-    if (table_used == 2) AttackTable = AttackTable2;
-    if (table_used == 3) AttackTable = custom;
-
+	table_used = document.getElementById('attack table').value;
+	if (table_used == 1) AttackTable = AttackTable1;
+	if (table_used == 2) AttackTable = AttackTable2;
+	if (table_used == 3) AttackTable = custom;
 
 	let type_0 = document.getElementById('attack type 0').value;
 	let PC_0 = document.getElementById('PC 0').checked;
@@ -314,16 +315,19 @@ function calculate() {
 
 	let damage_0 = 0;
 	if (type_0 != -1) damage_0 = attackTable(type_0, combo_0, B2B_0);
-	if (PC_0) damage_0 += 10; // technically PC damage comes separately and that should be indicated but whatever
 
-	document.getElementById('damage 0').innerHTML = damage_0;
+	if (PC_0) {
+		if (damage_0 == 0) document.getElementById('damage 0').innerHTML = 10;
+		else document.getElementById('damage 0').innerHTML = damage_0.toString() + ' + 10';
+		damage_0 += 10;
+	} else document.getElementById('damage 0').innerHTML = damage_0;
 
 	let types = [type_0];
 	let combos = [combo_0];
 	let B2Bs = [B2B_0];
 	let damages = [damage_0];
 
-    for (i = 1; i < num_attacks; i++) {
+	for (i = 1; i < num_attacks; i++) {
 		let type = document.getElementById('attack type ' + i.toString()).value;
 		let PC = document.getElementById('PC ' + i.toString()).checked;
 
@@ -340,10 +344,13 @@ function calculate() {
 
 		let damage = 0;
 		if (type != -1) damage = attackTable(type, combo, B2B);
-		if (PC) damage += 10;
+		if (PC) {
+			if (damage == 0) document.getElementById('damage ' + i.toString()).innerHTML = 10;
+			else document.getElementById('damage ' + i.toString()).innerHTML = damage.toString() + ' + 10';
+			damage += 10;
+		} else document.getElementById('damage ' + i.toString()).innerHTML = damage;
 
 		damages.push(damage);
-		document.getElementById('damage ' + i.toString()).innerHTML = damage;
 	}
 
 	let total = damages.reduce((a, b) => a + b, 0); // sum
